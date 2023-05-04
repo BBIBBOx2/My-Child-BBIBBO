e80736763992824950e2584df989f50ab256ac49
subin1107 <btsiu@naver.com>
2023-05-05 02:54:34 +0900

    test

---
+++ src/main/java/com/publicapi/test/Controller/MainController.java
@@ -0,0 +1,17 @@
+package com.publicapi.test.Controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+    @GetMapping("/test")
+    @ResponseBody
+    public String index() {
+        return "hello";
+    }
+}
