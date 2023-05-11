dff05b34e7fdb37eec3b45495a12a29f63240538
subin1107 <btsiu@naver.com>
2023-05-12 00:33:58 +0900

    [feat] hospital list 출력 기능 구현

--- src/main/java/com/publicapi/test/domain/hospital/controller/HospitalController.java
+++ src/main/java/com/publicapi/test/domain/hospital/controller/HospitalController.java
@@ -1,9 +1,13 @@
 package com.publicapi.test.domain.hospital.controller;
 
+import com.publicapi.test.domain.hospital.entity.HospitalEntity;
 import com.publicapi.test.domain.hospital.service.HospitalService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,6 +23,13 @@ public class HospitalController {
     @GetMapping("/hospital")
     public String map() {
         return "hospital/hospital-map";
+    }
+
+    @GetMapping("/hospital/list")
+    public String hospitalList(Model model) {
+        List<HospitalEntity> hospitalList = hospitalService.getHospital();
+        model.addAttribute("hospitalList", hospitalList);
+        return "list/hospital_list";
     }
 
 }
--- src/main/java/com/publicapi/test/domain/hospital/controller/MapController.java
+++ src/main/java/com/publicapi/test/domain/hospital/controller/MapController.java
@@ -1,9 +1,12 @@
 package com.publicapi.test.domain.hospital.controller;
 
 import com.publicapi.test.domain.hospital.dto.HospitalDto;
+import com.publicapi.test.domain.hospital.entity.HospitalEntity;
 import com.publicapi.test.domain.hospital.service.HospitalService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,16 +15,6 @@ import java.util.List;
 
 @Slf4j
 @RestController
-import com.publicapi.test.domain.hospital.entity.HospitalEntity;
-import com.publicapi.test.domain.hospital.service.HospitalService;
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-
-import java.util.List;
-
-@Controller
 @RequiredArgsConstructor
 public class MapController {
 
@@ -33,18 +26,6 @@ public class MapController {
         List<HospitalDto> hospitals = hospitalService.getAllHospitals();
         System.out.println("hospitals = " + hospitals);
         return hospitals;
-    }
-
-    @GetMapping("/apiTest")
-    public void apiTest() {
-        hospitalService.syncHospitalInfo();
-    }
-
-    @GetMapping("/hospital/list")
-    public String hospitalList(Model model) {
-        List<HospitalEntity> hospitalList = hospitalService.getHospital();
-        model.addAttribute("hospitalList", hospitalList);
-        return "list/hospital_list";
     }
 
 }
