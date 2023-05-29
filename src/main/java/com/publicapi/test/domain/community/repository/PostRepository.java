02a3dde535cb3334100c9cf0c03bb7cf0363c8fd
JJ503 <63184334+JJ503@users.noreply.github.com>
2023-05-29 11:44:52 +0900

    [feat] 조회수 업데이트 기능 추가

--- src/main/java/com/publicapi/test/domain/community/repository/PostRepository.java
+++ src/main/java/com/publicapi/test/domain/community/repository/PostRepository.java
@@ -5,11 +5,18 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
     Page<Post> findByAuthorId(Long id, Pageable pageable);
-    Page<Post> findAll(Pageable pageable);
     Page<Post> findAll(Specification<Post> spec, Pageable pageable);
+
+    @Modifying
+    @Query("UPDATE Post post SET post.hits = (post.hits + 1) WHERE post.id = :postId")
+    void updatePostHits(@Param("postId") Long postId);
+
 }
